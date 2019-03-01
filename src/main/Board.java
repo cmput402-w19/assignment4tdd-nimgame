61290b7563345efa10f941455068ebed25912019
ricardodong <shutong@ualberta.ca>
2019-03-01 16:17:47 -0700

    init of project

---
+++ src/main/Board.java
@@ -0,0 +1,14 @@
+public class Board {
+
+    //fundmentally the game has to have three heaps and two fixed players
+    // number of stones in each heap should be decided by user
+    //there is no draw, but I want to predict the possible winner
+    //I will wirte the member function and vars before test.
+    private int heap[];
+    private enum player {A, B, NONE};
+    private player currPlayer;
+
+    public Board(){
+
+    }
+}
