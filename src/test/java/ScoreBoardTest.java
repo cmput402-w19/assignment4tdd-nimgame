cb19383c24506e1b05f3bdfb9c51405394d6922f
msa3d2023 <msa3d2023@fastmail.com>
2019-03-04 15:36:11 -0700

    RED - incrementNumOfWins

--- src/test/java/ScoreBoardTest.java
+++ src/test/java/ScoreBoardTest.java
@@ -43,4 +43,20 @@ public class ScoreBoardTest {
         assert(scoreBoard.getNumOfTies(ScoreBoard.Player.B) == 2);
         assert(scoreBoard.getNumOfTies(ScoreBoard.Player.NONE) == 0);
     }
+
+    @Test
+    public void testIncrementNumOfWins(){
+        int[][] scores = new int[2][3];
+        scores[0][0] = 1;
+        scores[1][0] = 2;
+        scoreBoard = new ScoreBoard(scores);
+
+        scoreBoard.incrementNumOfWins(ScoreBoard.Player.A);
+        scoreBoard.incrementNumOfWins(ScoreBoard.Player.B);
+        scoreBoard.incrementNumOfWins(ScoreBoard.Player.NONE);
+
+        assert(scoreBoard.getNumOfWins(ScoreBoard.Player.A) == 2);
+        assert(scoreBoard.getNumOfWins(ScoreBoard.Player.B) == 3);
+        assert(scoreBoard.getNumOfWins(ScoreBoard.Player.NONE) == 0);
+    }
 }
