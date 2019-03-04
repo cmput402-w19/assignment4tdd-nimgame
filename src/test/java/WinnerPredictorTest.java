76f3de5d180a833900408c9f00c3e6d5db5232ac
ricardodong <shutong@ualberta.ca>
2019-03-04 12:20:04 -0700

    RED, issue#5

---
+++ src/test/java/WinnerPredictorTest.java
@@ -0,0 +1,64 @@
+import exceptions.InvalidPickException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class WinnerPredictorTest {
+
+    private WinnerPredictor winnerPredictor;
+
+    @Before
+    public void setup() {
+        winnerPredictor = new WinnerPredictor();
+
+    }
+
+    @Test
+    public void testInit(){
+
+    }
+
+    @Test
+    public void testPredictWinner() throws InvalidPickException{
+        int[] testState1 = {3, 4, 5};
+        int[] testState2 = {1, 4, 5};
+        Board playingBoard = mock(Board.class);
+        when(playingBoard.getCurrPlayer()).thenReturn(Board.player.A);
+        when(playingBoard.getHeap()).thenReturn(testState1).thenReturn(testState2);
+
+        Board.player predictWinner = winnerPredictor.PredictWinner(playingBoard.getHeap(), playingBoard.getCurrPlayer());
+        assert (predictWinner.equals(Board.player.A));
+
+        predictWinner = winnerPredictor.PredictWinner(playingBoard.getHeap(), playingBoard.getCurrPlayer());
+        assert (predictWinner.equals(Board.player.B));
+    }
+
+    @Test
+    public void testGoodMoveChecker() throws InvalidPickException{
+        // these are states after one move from [3,4,5]
+        int[] testState1 = {2, 4, 5};
+        int[] testState2 = {1, 4, 5};
+        Board playingBoard = mock(Board.class);
+        when(playingBoard.getCurrPlayer()).thenReturn(Board.player.B);
+        when(playingBoard.getHeap()).thenReturn(testState1).thenReturn(testState2);
+
+        assert (winnerPredictor.getPrePredictWinner().equals(Board.player.A));
+        assert (!winnerPredictor.GoodMoveCheck(playingBoard.getHeap()));
+        assert (winnerPredictor.GoodMoveCheck(playingBoard.getHeap()));
+
+    }
+
+    @Test
+    public void testGetGoodMove() throws InvalidPickException{
+        int[] testState1 = {0, 3, 2};
+        int[] goalState = {0, 2, 2};
+
+        assert (Arrays.equals(winnerPredictor.getPreState(), testState1));
+        assert (Arrays.equals(winnerPredictor.GetGoodMove(), goalState));
+
+    }
+}
